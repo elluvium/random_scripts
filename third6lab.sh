87915e138abaaae7fc775c228b19b394e9e63f35
Stanislav <kostenkostas@gmail.com>
2016-11-12 14:19:12 +0000

    fixed third6lab.sh

--- third6lab.sh
+++ third6lab.sh
@@ -6,14 +6,34 @@ read -n 1 -p "Are you sure to run this script? (y/[a]): " AREYOUSURE;
 [ "$AREYOUSURE" = "y" ] || exit;
 echo "" #for next line
 #___________________________________________________________________
+
 echo "Do you know the type of your OS?"; read first_answer;
-case $first_answer in
-	'No')
+case "$first_answer" in
+	"No" | "no" | "n" | "N")
+           echo ""
 	   echo "Do you want to know your OS type?"; read second_answer;
-		
+		case "$second_answer" in
+		"No" | "no" | "n" | "N")
+			exit;;
+		"yes" | "Yes" | "y" | "Y")
+			OS=$(lsb_release -sd); OS_str=$(uname -a);
+			echo "Distributive: $OS"; echo "Kernel version: $OS_str" ;;
+	        esac ;;
 
+	"yes" | "Yes" | "Y" | "y")
+		OS=$(lsb_release -sd); OS_str=$(uname -a);
 
-OS_VER=$(lsb_release -sd)
-echo $OS_VER
+		echo "Type it" ; read user_variant;
+
+		if echo "$OS_str" | grep -qi "$user_variant"
+                then
+			echo "You're right! You have a $OS";
+		else
+			echo "You're wrong! You have a $OS"
+		fi
+esac
+#OS_VER=$(lsb_release -sd)
+
+#echo $OS_VER
 
 
